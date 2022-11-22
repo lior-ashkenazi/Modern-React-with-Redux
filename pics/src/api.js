d13c92610468f97dab801e31d87e60367b3d8c7a
Lior Ashkenazi <lior.ashkenazi>
2022-11-22 22:38:25 +0200

    Completed Section 5

--- pics/src/api.js
+++ pics/src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
       headers:{
-        Authorization: 'Client-ID QB1v26gHO0WmqSSz2mJhQAv78tmYHsR9iW4hkTc1F2s'
+        Authorization: 'Client-ID QB1v26gHO0WmqSSz2mJhQAv78tmYHsR9iW4hkTc1F2s',
       },
       params: {
-        query: 'cars'
+        query: term,
       }
   })
-    console.log(response);
 
-    return response;
+    return response.data.results;
 }
 
 export default searchImages;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,5 +1,31 @@
-function SearchBar() {
-    return <div>SearchBar</div>
+import './SearchBar.css';
+import {useState} from 'react';
+
+function SearchBar({onSubmit}) {
+    const [term, setTerm] = useState('');
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        onSubmit(
+            // NEVER EVER EVER DO THIS
+            // document.querySelector('input').value
+            term
+        );
+    };
+
+    const handleChange = (event) => {
+        setTerm(event.target.value);
+    };
+
+    return (
+        <div className="search-bar">
+            <form onSubmit={handleFormSubmit}>
+                <label>Enter Search Term</label>
+                <input value={term} onChange={handleChange}/>
+            </form>
+        </div>
+    );
 }
 
-export default SearchBar
+export default SearchBar;
