261883bff6c06ecca70fa38aaf7538f4bd35191e
Lior Ashkenazi <lior.ashkenazi>
2022-12-07 14:00:42 +0200

    Completed Section 13

--- comps/src/index.js
+++ comps/src/index.js
@@ -1,9 +1,10 @@
-import './index.css'
+import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {NavigationProvider} from './context/navigation';
 
-const el = document.getElementById('root')
-const root = ReactDOM.createRoot(el)
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
 
-root.render(<App/>)
+root.render(<NavigationProvider><App/></NavigationProvider>);
